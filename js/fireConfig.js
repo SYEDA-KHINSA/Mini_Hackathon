04cb4d3240b0be511c212d81704f445a3ff44416
SYEDA KHINSA <khinsamukhtar.24@gmail.com>
2023-09-10 22:12:01 +0500

    Mini_hackathon

---
+++ js/fireConfig.js
@@ -0,0 +1,17 @@
+// import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDAc4V1C1ceGi3otn4iLy6cBlXciiQXXy0",
+    authDomain: "mini-hackathon-68369.firebaseapp.com",
+    databaseURL: "https://mini-hackathon-68369-default-rtdb.firebaseio.com",
+    projectId: "mini-hackathon-68369",
+    storageBucket: "mini-hackathon-68369.appspot.com",
+    messagingSenderId: "995265421520",
+    appId: "1:995265421520:web:d53bb7ce6d9ab427b97ed5",
+    measurementId: "G-S91J2ZQPT1"
+  };
+  
+    
+    // Initialize Firebase
+    // const app = initializeApp(firebaseConfig);
+    firebase.initializeApp(fireConfig);
